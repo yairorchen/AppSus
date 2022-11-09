f9396446a96137b14debefcf917dc5b410169f02
Yair Orchen <yairorchen1@gmail.com>
2022-11-09 12:56:29 +0200

    starge fixing

--- apps/keep/cmps/note-list.cmp.js
+++ apps/keep/cmps/note-list.cmp.js
@@ -1,6 +1,7 @@
 // import notePreview from './note-preview.cmp.js'
 
 export default {
+  name: 'note-list',
   template: `
     <section>
      <h3>list</h3>
--- services/util.service.js
+++ services/util.service.js
@@ -1,13 +1,24 @@
 export const utilService = {
-    makeId
+  makeId,
+  saveToStorage,
+  loadFromStorage,
 }
 
+function saveToStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value) || null)
+}
+
+function loadFromStorage(key) {
+  let data = localStorage.getItem(key)
+  return data ? JSON.parse(data) : undefined
+}
 
 function makeId(length = 5) {
-    var txt = ''
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    for (var i = 0; i < length; i++) {
-        txt += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
-    return txt
+  var txt = ''
+  var possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return txt
 }
