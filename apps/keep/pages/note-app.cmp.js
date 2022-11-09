d2ed5fea9e9e02f2fd95c029fbcaf209dc23eae5
Yair Orchen <yairorchen1@gmail.com>
2022-11-09 11:48:08 +0200

    basic wiring

---
+++ apps/keep/pages/note-app.cmp.js
@@ -0,0 +1,7 @@
+export default {
+  template: `
+        <section >
+            <h1>note app</h1>
+        </section>
+    `,
+}
--- routes.js
+++ routes.js
@@ -1,20 +1,30 @@
 import homePage from './views/app-home.cmp.js'
 import aboutPage from './views/app-about.cmp.js'
+import keep from './apps/keep/pages/note-app.cmp.js'
+import mail from './apps/mail/pages/mail-app.cmp.js'
 
 const { createRouter, createWebHashHistory } = VueRouter
 
 const routerOptions = {
-	history: createWebHashHistory(),
-	routes: [
-		{
-			path: '/',
-			component: homePage,
-		},
-		{
-			path: '/about',
-			component: aboutPage,
-		},
-	],
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      component: homePage,
+    },
+    {
+      path: '/about',
+      component: aboutPage,
+    },
+    {
+      path: '/keep',
+      component: keep,
+    },
+    {
+      path: '/mail',
+      component: mail,
+    },
+  ],
 }
 
 export const router = createRouter(routerOptions)
