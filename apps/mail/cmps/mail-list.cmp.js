74fe5c1b0b980340ee6300c4ca754f5708d49066
Boaz Deri <boazderi@gmail.com>
2022-11-09 16:26:12 +0200

    improve naviggation between details and list

--- apps/mail/cmps/mail-list.cmp.js
+++ apps/mail/cmps/mail-list.cmp.js
@@ -4,21 +4,23 @@ export default {
     name: 'mail-list',
     props: ['mails'],
     template: `
+    <section v-if="!isClicked">
         <h1> mail list home</h1>
         <ul class="clean-list">
             <li  v-for="mail in mails" :key="mail.id" class="mail-line flex justify-between">
-            <mail-preview :mail="mail"/>
-            <router-link :to="{name:'details',params:{id:mail.id},query:{name:mail.name}}">Details</router-link>
+                <mail-preview  :mail="mail"/>
+                <router-link @click="toggleClick" :to="{name:'details',params:{id:mail.id},query:{name:mail.name}}">Details</router-link>
             </li>
         </ul>
+    </section>
     `,
     data() {
         return {
-
+            isClicked: null
         }
     },
     created() {
-        
+        this.isClicked=false
     },
     mounted() {
         console.log('mails', this.mails);
@@ -26,6 +28,10 @@ export default {
     methods: {
         openEmail(){
             console.log('open email!');
+        },
+        toggleClick(){
+            console.log('toggle click');
+            this.isClicked = true
         }
     },
     computed: {
