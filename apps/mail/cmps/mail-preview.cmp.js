9b32afe636efa499de34eeac8b597895cdbf56f8
Boaz Deri <boazderi@gmail.com>
2022-11-11 14:57:50 +0200

    update imgs path for github pages

--- apps/mail/cmps/mail-preview.cmp.js
+++ apps/mail/cmps/mail-preview.cmp.js
@@ -2,7 +2,7 @@ export default {
     name: 'mail-preview',
     props: ['mail'],
     template: `
-    <div @mouseover="isHoverToggle" @mouseleave="isNotHoverToggle" class="mail-line flex justify-between" :class="{'mail-read': mail.isRead}" >
+    <article @mouseover="isHoverToggle" @mouseleave="isNotHoverToggle" class="mail-line flex justify-between" :class="{'mail-read': mail.isRead}" >
         <div class="start-line flex ">
             <div class="name"> {{ mail.name }} </div>
             <div class="subject"> {{ mail.subject }}</div>
@@ -14,16 +14,16 @@ export default {
            v-if="isHover">
             <button v-if="!mail.isRead" title="Mark as read" @click.stop.prevent="toggleRead()"
              class="is-read-btn mail-line-btn">
-                <img src="./../assets/img/open-mail.png" width=24 height=18 />
+                <img src="./assets/img/open-mail.png" width=24 height=18 />
             </button>
             <button v-if="mail.isRead" title="Mark as not read" @click.stop.prevent="toggleRead()" class="is-read-btn mail-line-btn">
-                <img src="./../assets/img/gmail.png" width=24 height=18  />
+                <img src="./assets/img/gmail.png" width=24 height=18  />
             </button>
             <button class="delete-btn mail-line-btn" @click.stop.prevent="remove">
-                <img title="Delete"  src="./../assets/img/delete.png"  width=14 height=18 alt=""/>
+                <img title="Delete"  src="./assets/img/delete.png"  width=14 height=18 alt=""/>
             </button>
         </div>
-    </div>
+    </article>
     `,
     data() {
         return {
