30d3ac309a251ba3e004381dd79b4a1909aee009
Yair Orchen <yairorchen1@gmail.com>
2022-11-11 19:26:55 +0200

    fixing the bugs in the basic features

--- apps/keep/cmps/note-list.cmp.js
+++ apps/keep/cmps/note-list.cmp.js
@@ -6,23 +6,33 @@ export default {
   name: 'note-list',
   template: `
     <!-- <router-link  to='/keep/add'><div @click="toggleShown()" v-if="isShow" class="new-note-link">Wright a note...</div></router-link> -->
-    <section class="notes-layout">
+    <section class="notes-layout ">
+<note-pinned 
+@save="save"
+@remove="remove"
+ :notes="notes" />
+
      <div class="notes-container">
 
-    <!-- <note-pinned :notes="notes" /> -->
-
-  <div class="note-preview" v-for="note in notes" :key="note.id" :style="note.style" >
+  <div v-for="note in notes" :key="note.id" >
+      <div  class="note-preview" v-if="!note.isPinned"  :style="note.style">
               <div class="black-white pointer" @click="togglePin(note.id)">📌</div>
                 <router-link :to="'/keep/' + note.id">
                 <note-preview :note="note" /> 
                 </router-link>
-              <div class="hover-show flex justify-between">
+               <div class="hover-show flex justify-between">
+                <div class="dots flex"  @click="toggleOptionOpen()">︙</div>
+                <!-- <div class="dots flex"  @click="remove(note.id)">︙</div> -->
                 <button class="imp black-white"> <input v-model="color" class="color-input" type="color"
                    @change="changeColor(note.id)" title="background color">🎨
                 </button>
-                <div class="dots flex"  @click="remove(note.id)">︙</div>
+                <div class="notes-option" :class="{hide:!optionOpen}">
+                  <p class="pointer option-item" @click="remove(note.id)">delete</p>
+                  <p class="pointer option-item">copy</p>
+                </div>
               </div>  
 
+          </div>
           </div>
       
      </div>
@@ -34,44 +44,42 @@ export default {
   },
   data() {
     return {
-      notesToShow: this.notes,
       isShow: true,
       color: '#ebfeff',
+      optionOpen: false,
     }
   },
   methods: {
     remove(noteId) {
       this.$emit('remove', noteId)
+      this.toggleOptionOpen()
     },
     toggleShown() {
       this.isShow = false
     },
+    toggleOptionOpen() {
+      this.optionOpen = !this.optionOpen
+    },
     changeColor(noteId) {
-      console.log(this.color)
-      console.log(noteId)
-      const idx = this.notesToShow.findIndex((note) => note.id === noteId)
-      this.notesToShow[idx].style = { backgroundColor: this.color }
-      this.save(this.notesToShow[idx])
+      const idx = this.notes.findIndex((note) => note.id === noteId)
+      this.notes[idx].style = { backgroundColor: this.color }
+      this.save(this.notes[idx])
     },
     save(note) {
       this.$emit('save', note)
     },
     togglePin(noteId) {
-      console.log(noteId)
-      const idx = this.notesToShow.findIndex((note) => note.id === noteId)
-      if (this.notesToShow[idx].isPinned === false) {
-        this.notesToShow[idx].isPinned = true
+      const idx = this.notes.findIndex((note) => note.id === noteId)
+      if (this.notes[idx].isPinned === false) {
+        this.notes[idx].isPinned = true
       } else {
-        this.notesToShow[idx].isPinned = false
+        this.notes[idx].isPinned = false
       }
-      this.save(this.notesToShow[idx])
-      console.log(this.notesToShow[idx])
+      this.save(this.notes[idx])
     },
   },
   computed: {
-    update() {
-      this.notesToShow = this.notes
-    },
+    update() {},
   },
   components: {
     notePreview,
