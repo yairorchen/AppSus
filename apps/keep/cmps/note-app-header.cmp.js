cb35a0b427d91bf772117eb320e15d0750940c46
Yair Orchen <yairorchen1@gmail.com>
2022-11-12 17:29:56 +0200

    checking git in the smartphone

--- apps/keep/cmps/note-app-header.cmp.js
+++ apps/keep/cmps/note-app-header.cmp.js
@@ -40,7 +40,7 @@ export default {
             
             <div class="place-for-icon"></div>
             
-            <img  @click="toggleSearch()" class="search-icon pointer note-search-icon" src="../assets/img/search-icon.png">
+            <img  @click="toggleSearch()" class="search-icon pointer note-search-icon" src="./assets/img/search-icon.png">
             <!-- <div>
             <select class="type-filter pointer" @change="filter" name="type" id="Type" v-model = "filterBy.type">
               <option value="note-txt">Text</option>
@@ -51,7 +51,7 @@ export default {
             </select>
         </div> -->
             <div>
-            <img class="candy-box-menu pointer" @click="toggleMenu()" src="../assets/img/candy-box-menu.png">
+            <img class="candy-box-menu pointer" @click="toggleMenu()" src="./assets/img/candy-box-menu.png">
             <nav v-if="menuOpen" class="menu-modal">
                 <router-link to="/" @click="toggleMenu()"><h3>Home</h3></router-link> | 
                 <router-link to="/about" @click="toggleMenu()"><h3>About</h3></router-link> |
